4bc016ccb6ed6f427e2d874c28b02976b182f534
Jim Doyle <indellibleink@gmail.com>
2020-11-12 07:07:20 -0500

    trying to add up cards per hand

--- Blackjack/src/com/careerdevs/objects/Dealer.java
+++ Blackjack/src/com/careerdevs/objects/Dealer.java
@@ -7,6 +7,7 @@ import com.careerdevs.game.GameThree;
 
 public class Dealer {
 
+	private static int sum = 0;
 	private static List<Card> hand = new ArrayList<>();
 	private static List<Card> dealer = new ArrayList<>();
 
@@ -15,23 +16,35 @@ public class Dealer {
 		deck.shuffle();
 		for (int idx = 0; idx < 4; idx++) {
 			Card topCard = deck.draw();
+			int cardInt=topCard.getValue();
 			if (idx % 2 == 0) {
 				hand.add(topCard);
 			} else {  
 				dealer.add(topCard);
+				
 			}
 		}
 		System.out.println("Hand");
 		for (Card card : hand) {
+			int sum=addHand(card);
+			System.out.println(card.toString()+"       "+sum);
 			
-			
-			System.out.println(card.toString());
 		}
 		
 		System.out.println("Dealer");
 		for (Card card : dealer) {
-			System.out.println(card.toString());
+			int sum2=addHand(card);
+			System.out.println(card.toString()+"       "+sum2);
 		}
 		
+	}
+	public static  int addHand(Card card) {
+			
+			int i=card.getValue();
+			if (i > 10) {
+				i = 10;
+			}
+			sum += i;
+		return sum;
 	}
 }
