fac6c4856ff0e67cf5e8998971f4b674e6b2fa69
Jim Doyle <indellibleink@gmail.com>
2020-11-23 18:39:39 -0500

    commented to change code to universal

--- Blackjack/src/com/careerdevs/objects/Moves.java
+++ Blackjack/src/com/careerdevs/objects/Moves.java
@@ -17,13 +17,17 @@ public class Moves {
 			int handTotal = Dealer.addUpCards(playerHand);
 			if (handTotal > 21) {
 				Console.bust(handTotal);
-				if (chips == 0) {
-					Console.gameOver();
-				}
+				
+//				Move this if statement out of code and move it somewhere else
+//				to make the code universal for player and dealer.
+//				if (chips == 0) {
+//					Console.gameOver();
+//				}
 				break;
 			}
 			Console.consoleHit(handTotal);
-
+			
+//			Make a switch/case or IF statement to determine if it's a dealer or player
 			String choice = Input.inputString();
 			if (choice.toLowerCase().equals("h")) {
 				Dealer.hit(deck, playerHand);
